d09249849db4f028e1ee9c92b0da65b71e13b61e
lzl <1158884621@qq.com>
2021-02-03 16:01:22 +0800

    color

--- TestDemo1/SecondViewController.swift
+++ TestDemo1/SecondViewController.swift
@@ -13,6 +13,8 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
     
 
