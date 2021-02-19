efcbb3e1f0bccf15811db3348ffce78f96d14349
Lzlwind <jysf1018@163.com>
2021-02-19 16:58:27 +0800

    thirdVC

---
+++ TestDemo1/ThirdViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThirdViewController.swift
+//  TestDemo1
+//
+//  Created by sky on 2021/2/19.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    lazy var label: UILabel = {
+        let label = UILabel.init(frame: CGRect(x: 100, y: 100, width: 60, height: 30))
+        label.text = "你好好不好";
+        label.textColor = UIColor.gray
+        
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
